ff7979132ebedb2cef2bab507af4f0fab5cf2bcb
NicholasTan24 <nicholastan_2327240005@mhs.mdp.ac.id>
2024-10-26 09:28:05 +0700

    Buat Profile Screen
    Membuat sebagian profile screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:wisata_candi_nicholastan/data/candi_data.dart';
 import 'package:wisata_candi_nicholastan/screens/detail_screen.dart';
+import 'package:wisata_candi_nicholastan/screens/profile_screen.dart';
 
 
 void main() {
@@ -19,7 +20,8 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: DetailScreen(candi: candiList[0]),
+      home: ProfileScreen(),
+      // home: DetailScreen(candi: candiList[0]),
     );
   }
 }
