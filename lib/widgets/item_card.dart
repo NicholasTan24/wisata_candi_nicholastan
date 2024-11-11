cdb4f338e931fb971bc91521efd22ae4f4f84dc2
NicholasTan24 <nicholastan_2327240005@mhs.mdp.ac.id>
2024-11-11 12:48:37 +0700

    HomeScreen
    melanjutkan bagian HomeScreen bagian textnya

--- lib/widgets/item_card.dart
+++ lib/widgets/item_card.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import '../models/candi.dart';
 
-
 class ItemCard extends StatelessWidget {
   // TODO: 1. Deklarasikan variabel yang dibutuhkan dan pasang pada konstruktor
   final Candi candi;
+
   const ItemCard({super.key, required this.candi});
 
   @override
@@ -20,18 +20,22 @@ class ItemCard extends StatelessWidget {
           // TODO: 3. Buat Image debagai anak dari Column
           Expanded(
             child: ClipRRect(
-              borderRadius: BorderRadius.circular(15),
-                child: Image.asset(
-                    candi.imageAsset, 
-                    width: double.infinity, 
-                    fit: BoxFit.cover
-                )
-            ),
+                borderRadius: BorderRadius.circular(15),
+                child: Image.asset(candi.imageAsset,
+                    width: double.infinity, fit: BoxFit.cover)),
           ),
           // TODO: 4. Buat Text sebagai anak dari Column
-          // Text(candi.name, style:),
+          Padding(
+            padding: EdgeInsets.only(left: 16, top: 8),
+            child: Text(candi.name,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+          ),
           // TODO: 5. Buat Text sebagai anak dari Column
-          // Text(data),
+          Padding(
+            padding: EdgeInsets.only(left: 16, bottom: 8),
+            child: Text(candi.type,
+                style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold)),
+          ),
         ],
       ),
     );
