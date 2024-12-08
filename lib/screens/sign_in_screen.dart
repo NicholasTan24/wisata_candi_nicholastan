aed339c8d424e6410ba5c854abab1146b6a6858d
NicholasTan24 <nicholastan_2327240005@mhs.mdp.ac.id>
2024-12-08 09:26:29 +0700

    Perbaikan Method Sign In
    Memperbaiki method sign in dan penambahan map sharedpreferences

--- lib/screens/sign_in_screen.dart
+++ lib/screens/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:encrypt/encrypt.dart' as encrypt;
 
 class SignInScreen extends StatefulWidget {
   SignInScreen({super.key});
@@ -21,48 +22,61 @@ class _SignInScreenState extends State<SignInScreen> {
 
   bool _obscurePassword = true;
 
+  Future<Map<String, String>> _retrieveAndDecryptDataFromPrefs(
+      Future<SharedPreferences> prefs)
+  async{
+    final sharedPreferences = await prefs;
+    final encryptedUsername = sharedPreferences.getString('username')?? '';
+    final encryptedPassword = sharedPreferences.getString('password')?? '';
+    final keyString = sharedPreferences.getString('key')?? '';
+    final ivString = sharedPreferences.getString('iv')?? '';
+    final encrypt.Key key = encrypt.Key.fromBase64(keyString);
+    final iv = encrypt.IV.fromBase64(ivString);
+    final encrypter = encrypt.Encrypter(encrypt.AES(key));
+    final decrytedUsername = encrypter.decrypt64(encryptedUsername, iv: iv);
+    final decryptedPassword = encrypter.decrypt64(encryptedPassword, iv: iv);
+    //mengembalikan data terdekripsi
+    return{'username': decrytedUsername, 'password':decryptedPassword};
+  }
   void _signIn() async {
-    final SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String savedUsername = prefs.getString('Username') ?? '';
-    final String savedPassword = prefs.getString('Password') ?? '';
-    final String enteredUsername = _usernameController.text.trim();
-    final String enteredPassword = _passwordController.text.trim();
-
-    if (enteredUsername.isEmpty || enteredPassword.isEmpty) {
-      setState(() {
-        _errorText = 'Nama pengguna dan kata sandi harus diisi.';
-      });
-      return;
-    }
-
-    if (savedUsername.isEmpty || savedPassword.isEmpty) {
-      setState(() {
-        _errorText =
-            'Pengguna belum terdaftar. Silahkan daftar terlebih dahulu.';
-      });
-      return;
-    }
-
-    if (enteredUsername == savedUsername && enteredPassword == savedPassword) {
-      setState(() {
-        _errorText = '';
-        _isSignedIn = true;
-        prefs.setBool('isSignedIn', true);
-      });
-      //Pemanggilan untuk menghapus semua halaman dalam tumpukan navigasi
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        Navigator.of(context).popUntil((route) => route.isFirst);
-      });
-      //Sign In berhasil, navigasikan ke layar utama
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        Navigator.pushReplacementNamed(context, '/');
-      });
-    } else {
-      setState(() {
-        _errorText = 'Nama pengguna atau kata sandi salah.';
-      });
+    try{
+      final Future<SharedPreferences> prefsFuture = SharedPreferences.getInstance();
+      final String username = _usernameController.text;
+      final String password = _passwordController.text;
+      print('Sign in attempt');
+      if (username.isNotEmpty && password.isNotEmpty) {
+        final SharedPreferences prefs = await prefsFuture;
+        final data = await _retrieveAndDecryptDataFromPrefs(prefs as Future<SharedPreferences>);
+        if (data.isNotEmpty) {
+          final decryptedUsername = data['username'];
+          final decryptedPassword = data['password'];
+          if (username == decryptedUsername && password == decryptedPassword) {
+            _errorText = '';
+            _isSignedIn = true;
+            prefs.setBool('isSignedIn', true);
+            // pemanggil untuk menghapus semua halaman dalam tumpukan navigasi
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              Navigator.of(context).popUntil((route) => route.isFirst);
+            });
+            //sign in berhasil, navigasikan ke layar utama
+            WidgetsBinding.instance.addPostFrameCallback((_) {
+              Navigator.pushReplacementNamed(context, '/');
+            });
+            print('Sign in succeeded');
+          } else {
+            print('Username or password is incorrect');
+          }
+        } else {
+          print('No stored credentials found');
+        }
+      }else{
+        print('Username and password cannot be empty');
+      }
+    }catch(e){
+      print('An error occurred: $e');
     }
   }
+
 
   @override
   Widget build(BuildContext context) {
--- lib/screens/sign_up_screen.dart
+++ lib/screens/sign_up_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
+import 'package:encrypt/encrypt.dart' as encrypt;
 
 class SignUpScreen extends StatefulWidget {
   SignUpScreen({super.key});
@@ -20,7 +20,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
 
   bool _obscurePassword = true;
 
-  //TODO: 1.Membuat method _signUp
+  //TODO: 2.Membuat method _signUp
   void _signUp()async{
     final SharedPreferences prefs = await SharedPreferences.getInstance();
     final String name = _fullnameController.text.trim();
@@ -37,17 +37,34 @@ class _SignUpScreenState extends State<SignUpScreen> {
       });
       return;
     }
+    //TODO : 3.Jika nama, username, password tidak kosong lakukan enkripsi
+    if(name.isNotEmpty && username.isNotEmpty && password.isNotEmpty){
+      final encrypt.Key key = encrypt.Key.fromLength(32);
+      final iv = encrypt.IV.fromLength(16);
+      final encrypter = encrypt.Encrypter(encrypt.AES(key));
+      final encryptedName = encrypter.encrypt(name, iv: iv);
+      final encryptedUsername = encrypter.encrypt(name, iv: iv);
+      final encryptedPassword = encrypter.encrypt(name, iv: iv);
+
+      //simpan data pengguna di SharedPreferences
+      prefs.setString('fullname', encryptedName.base64);
+      prefs.setString('username', encryptedUsername.base64);
+      prefs.setString('password', encryptedPassword.base64);
+      prefs.setString('key', key.base64);
+      prefs.setString('iv', iv.base64);
+
+    }
     //simpan data pengguna di SharedPreferences
-    prefs.setString('Full Name', name);
-    prefs.setString('Username', username);
-    prefs.setString('Password', password);
+    // prefs.setString('Full Name', name);
+    // prefs.setString('Username', username);
+    // prefs.setString('Password', password);
 
     //buat navigasi ke signinscreen
     Navigator.pushReplacementNamed(context, '/SignInScreen');
   }
 
 
-  //TODO: 2.Membuat method dispose
+  //TODO: 4.Membuat method dispose
   @override
   void dispose(){
     //TODO: Implement dispose
